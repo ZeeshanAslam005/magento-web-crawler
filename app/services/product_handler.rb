42c2ac1413551c4597a3cab13756bc3d44e57b84
Zeeshan Aslam <zshan005@gmail.com>
2020-08-16 23:30:50 +0500

    encapsualte private methods

--- app/services/product_handler.rb
+++ app/services/product_handler.rb
@@ -6,27 +6,6 @@ class ProductHandler
     @href = href
   end
 
-  def document
-    @document ||= begin
-        Nokogiri::HTML(open(@href))
-    end
-  end
-
-  def name
-    document.css('main.page-main h1.page-title span.base').text
-  end
-
-  def price
-    document.css('main.page-main div.product-info-price span.price').text
-  end
-
-  def details
-    document.css('main.page-main div.product.attribute.description').text
-  end
-
-  def extra_information
-    document.css('main.page-main #product-attribute-specs-table tbody').text
-  end
 
   def parse
     @name = name
@@ -56,14 +35,39 @@ class ProductHandler
     print
   end
 
-  def related_hrefs
-    hrefs = document.css('ol.products.list.items.product-items').css('a')
-     hrefs.map { |element| element['href'] }.compact.uniq - ['#']
-  end
 
   def save_realted_links
     related_hrefs.each do |href|
       Product.find_or_create_by(href: href) #only create if link not already exist
     end
   end
+
+  private
+
+   def document
+      @document ||= begin
+         Nokogiri::HTML(open(@href))
+      end
+   end
+
+   def name
+      document.css('main.page-main h1.page-title span.base').text
+   end
+
+   def price
+      document.css('main.page-main div.product-info-price span.price').text
+   end
+
+   def details
+      document.css('main.page-main div.product.attribute.description').text
+   end
+
+   def extra_information
+      document.css('main.page-main #product-attribute-specs-table tbody').text
+   end
+
+   def related_hrefs
+      hrefs = document.css('ol.products.list.items.product-items').css('a')
+       hrefs.map { |element| element['href'] }.compact.uniq - ['#']
+   end
 end
